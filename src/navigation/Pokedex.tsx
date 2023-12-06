a1e0cacc5df88ef71132513e2172918299f8225b
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2023-12-05 23:49:53 -0400

    Customized pokemon detail header with arrow icon and favourite button

--- src/navigation/Pokedex.tsx
+++ src/navigation/Pokedex.tsx
@@ -1,5 +1,7 @@
+import Icon from '@expo/vector-icons/MaterialIcons';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { Pressable } from 'react-native';
 
 import { PokedexStackParamsList } from './nav-params';
 import Pokedex from '../screens/Pokedex';
@@ -18,11 +20,19 @@ export default function PokedexNavigation() {
       <Stack.Screen
         name="Pokemon"
         component={Pokemon}
-        options={{
+        options={({ route }) => ({
           title: '',
           headerTransparent: true,
-          //headerTintColor: 'white',
-        }}
+          headerRight: () => (
+            <Pressable
+              onPress={() => {
+                console.log({ ID: route.params.id });
+              }}>
+              <Icon name="favorite-outline" size={24} color="white" />
+            </Pressable>
+          ),
+          headerTintColor: 'white',
+        })}
       />
     </Stack.Navigator>
   );
