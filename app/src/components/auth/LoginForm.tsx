3ff90745087fb5f57dcfa7e630888e23d187a3f1
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2023-12-10 13:05:47 -0400

    APP: added formik and yup for form state handling and validations

--- app/src/components/auth/LoginForm.tsx
+++ app/src/components/auth/LoginForm.tsx
@@ -1,20 +1,62 @@
+import { useFormik } from 'formik';
 import React from 'react';
 import { View, Text, StyleSheet, Button, Keyboard, TextInput } from 'react-native';
+import * as Yup from 'yup';
 
 import { CustomButton } from '../Button';
 
+interface Form {
+  user: string;
+  password: string;
+}
+
+const initialValues: Form = {
+  user: '',
+  password: '',
+};
+
+const validationSchema = Yup.object<Form>().shape({
+  user: Yup.string().required('Please enter your username'),
+  password: Yup.string().required('Please enter  your password').min(5, 'Invalid password length'),
+});
+// TODO: run this and see how it looks
 export default function LoginForm() {
+  const formik = useFormik<Form>({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      console.log({ values });
+    },
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+  const handleSubmit = () => {
+    formik.handleSubmit();
+  };
   return (
     <View>
       <Text style={styles.title}>Login</Text>
-      <TextInput placeholder="Username" style={styles.input} autoCapitalize="none" />
+      <TextInput
+        placeholder="Username"
+        style={styles.input}
+        autoCapitalize="none"
+        value={formik.values.user}
+        onChangeText={formik.handleChange('user')}
+        onBlur={formik.handleBlur('user')}
+      />
+      <Text style={styles.error}> {formik.errors.user}</Text>
       <TextInput
         placeholder="Password"
         style={styles.input}
         autoCapitalize="none"
         secureTextEntry
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+        onBlur={formik.handleBlur('password')}
       />
-      <CustomButton onPress={() => {}} title="Sign In" />
+      <Text style={styles.error}> {formik.errors.password}</Text>
+
+      <CustomButton onPress={handleSubmit} title="Sign In" />
     </View>
   );
 }
@@ -33,5 +75,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
+  },
+  error: {
+    color: 'red',
+    textAlign: 'left',
+    marginTop: 10,
   },
 });
